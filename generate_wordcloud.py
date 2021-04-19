f31983acf3080a7d8396e2f1a195f3544d481a01
sejaldua <sejal.dua@tufts.edu>
2021-04-19 16:45:15 -0400

    play with masks

--- generate_wordcloud.py
+++ generate_wordcloud.py
@@ -1,6 +1,11 @@
 # Import packages
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS
+import numpy as np
+from PIL import Image
+
+
+
 
 # Define a function to plot word cloud
 def plot_cloud(wordcloud):
@@ -12,13 +17,16 @@ def plot_cloud(wordcloud):
     plt.axis("off");
 
 with open('transcripts/glass_animals_heat_waves.txt', 'r+') as f:
-    text = f.read()
+    text = f.read().lower()
+
+# Import image to np.array
+mask = np.array(Image.open('masks/dave_sticker.jpg'))
 
 # Generate word cloud
-wordcloud = WordCloud(width = 3000, height = 2000, random_state=42, background_color='black', colormap='Set2', collocations=True, stopwords = STOPWORDS).generate(text)
+wordcloud = WordCloud(width = 3000, height = 2000, random_state=42, background_color='black', colormap='Set2', collocations=True, stopwords = STOPWORDS, mask=mask).generate(text)
 # Plot
 wc = plot_cloud(wordcloud)
 # plt.show()
 
 # Save image
-wordcloud.to_file("wordclouds/glass_animals_heat_waves_wc.png")
+wordcloud.to_file("wordclouds/glass_animals_heat_waves_wc_mask.png")
